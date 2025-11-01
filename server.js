dab970c65adf61403a86a7be76fa61aa3c82dd48
Vaibhav Jagtap <vaibhavsocial7@gmail.com>
2025-11-01 17:20:30 +0530

    method added in server js in cors

--- server.js
+++ server.js
@@ -6,28 +6,31 @@ dotenv.config();
 import userRoute from "./routes/user.route";
 import adminRoute from "./routes/admin.route.js";
 import connectDB from "./config/db.js";
-import vehicleRoute from "./routes/vehicle.route.js"
+import vehicleRoute from "./routes/vehicle.route.js";
 import bookingRoute from "./routes/booking.route";
-import paymentRoute from "./routes/payment.route.js"
-import blogRoute from "./routes/blog.route.js"
-
+import paymentRoute from "./routes/payment.route.js";
+import blogRoute from "./routes/blog.route.js";
 
 // initialize express app
 const app = express();
-
-
 
 // db connect
 connectDB();
 
 // middleware
-app.use(cors({
-  origin: ["http://localhost:3000", "https://car-rental-front-end-sooty.vercel.app/"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://car-rental-front-end-sooty.vercel.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-
 
 const port = process.env.PORT || 5000;
 
@@ -37,9 +40,6 @@ app.use(process.env.API_CAR, vehicleRoute);
 app.use(process.env.API_BOOKING, bookingRoute);
 app.use(process.env.API_PAYMENT, paymentRoute);
 app.use(process.env.API_BLOG, blogRoute);
-
-
-
 
 app.get("/", (req, res) => res.send("Server is running"));
 
